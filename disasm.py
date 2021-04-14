386af0396929742f6a7a78f1e81bcd8761d24387
Michael Stöckel <michael.stoeckel@web.de>
2021-04-14 20:13:22 +0200

    changed output to have opcode-address and opcode as comment

--- disasm.py
+++ disasm.py
@@ -14,44 +14,41 @@ def dump(mem, size):
         print("")
         i = i+j+1
 
-def decode(hbyte, lbyte):
+def decode(hbyte, lbyte, opaddr):
     # extract some values which might come handy
     opcode = (hbyte << 8) | lbyte  # complete 2-byte opcode
-    nibble = hbyte >> 4            #
+    nibble = hbyte >> 4            # first hex-digit of opcode
 
     addr = opcode & 0x0FFF         # three byte address
     reg  = hbyte & 0x0F            # register number
     reg2 = lbyte >> 4              # register number for opcodes with two registers
     val  = lbyte                   # second byte as value
-    
-    # used in version 0.1
-    print("{:02x} {:02x}\t".format(hbyte, lbyte), end="")
-    
+        
     if nibble == 0x00:
         if lbyte == 0xE0:
-            print("CLS")
+            print("CLS", end="")
         elif lbyte == 0xEE:
-            print("RET")
+            print("RET", end="")
         else:
-            print("")
+            print("", end="")
     elif nibble == 0x01:
-        print("JP\t0x{:03x}".format(addr))
+        print("JP\t0x{:03x}".format(addr), end="")
     elif nibble == 0x02:
-        print("CALL\t0x{:03x}".format(addr))
+        print("CALL\t0x{:03x}".format(addr), end="")
     elif nibble == 0x03:
-        print("SE\tV{:0x}, {}".format(reg, val))
+        print("SE\tV{:0x}, {}".format(reg, val), end="")
     elif nibble == 0x04:
         # 4xkk - SNE Vx, byte
-        print("SNE\tV{:x}, {}".format(reg, val))
+        print("SNE\tV{:x}, {}".format(reg, val), end="")
     elif nibble == 0x05:
         # 5xy0 - SE Vx, Vy
-        print("SE\tV{:x}, V{:x}".format(reg, reg2))
+        print("SE\tV{:x}, V{:x}".format(reg, reg2), end="")
     elif nibble == 0x06:
         # 6xkk - LD Vx, byte
-        print("LD\tV{:x}, {}".format(reg, val))
+        print("LD\tV{:x}, {}".format(reg, val), end="")
     elif nibble == 0x07:
         # 7xkk - ADD Vx, byte
-        print("ADD\tV{:x}, {}".format(reg, val))
+        print("ADD\tV{:x}, {}".format(reg, val), end="")
     elif nibble == 0x08:
         selector = lbyte & 0x0F
 
@@ -82,72 +79,76 @@ def decode(hbyte, lbyte):
         if selector == 0xe:
             # 8xyE - SHL Vx {, Vy}
             print("SHL\t", end="")
-        print("V{:x}, V{:x}".format(reg, reg2))
+        print("V{:x}, V{:x}".format(reg, reg2), end="")
     elif nibble == 0x09:
         # 9xy0 - SNE Vx, Vy
-        print("SNE\tV{:x}, V{:x}".format(reg, reg2))
+        print("SNE\tV{:x}, V{:x}".format(reg, reg2), end="")
     elif nibble == 0x0A:
         # Annn - LD I, addr
-        print("LD\tI, 0x{:03x}".format(addr))
+        print("LD\tI, 0x{:03x}".format(addr), end="")
     elif nibble == 0x0B:
         # Bnnn - JP V0, addr
-        print("JP\tV0, 0x{:03x}".format(addr))
+        print("JP\tV0, 0x{:03x}".format(addr), end="")
     elif nibble == 0x0C:
         # Cxkk - RND Vx, byte
-        print("RND\tV{:x}, {}".format(reg, val))
+        print("RND\tV{:x}, {}".format(reg, val), end="")
     elif nibble == 0x0D:
         # Dxyn - DRW Vx, Vy, nibble
         n = lbyte & 0x0F
-        print("DRW\tV{:x}, V{:x}, {}".format(reg,reg2,n))
+        print("DRW\tV{:x}, V{:x}, {}".format(reg,reg2,n), end="")
     elif nibble == 0x0E:
         if lbyte == 0x9E:
             # Ex9E - SKP Vx
-            print("SKP\tV{:x}".format(reg))
+            print("SKP\tV{:x}".format(reg), end="")
         if lbyte == 0xA1:
             # ExA1 - SKNP Vx
-            print("SKNP\tv{:x}".format(reg))
+            print("SKNP\tv{:x}".format(reg), end="")
         else:
             print()
     elif nibble == 0x0F:
         if lbyte == 0x07:
             # Fx07 - LD Vx, DT
-            print("LD\tV{:x}, DT".format(reg))
+            print("LD\tV{:x}, DT".format(reg), end="")
         elif lbyte == 0x0A:
             # Fx0A - LD Vx, K
-            print("LD\tV{:x}, K".format(reg))
+            print("LD\tV{:x}, K".format(reg), end="")
         elif lbyte == 0x15:
             # Fx15 - LD DT, Vx
-            print("LD\tDT, V{:x}".format(reg))
+            print("LD\tDT, V{:x}".format(reg), end="")
         elif lbyte == 0x18:
             # Fx18 - LD ST, Vx
-            print("LD\tST, V{:x}".format(reg))
+            print("LD\tST, V{:x}".format(reg), end="")
         elif lbyte == 0x1E:
             # Fx1E - ADD I, Vx
-            print("ADD\tI, V{:x}".format(reg))
+            print("ADD\tI, V{:x}".format(reg), end="")
         elif lbyte == 0x29:
             # Fx29 - LD F, Vx
-            print("LD\tI, V{:x}".format(reg))
+            print("LD\tI, V{:x}".format(reg), end="")
         elif lbyte == 0x33:
             # Fx33 - LD B, Vx
-            print("LD\tB, V{:x}".format(reg))
+            print("LD\tB, V{:x}".format(reg), end="")
         elif lbyte == 0x55:
             # Fx55 - LD [I], Vx
-            print("LD\t[I], V{:x}".format(reg))
+            print("LD\t[I], V{:x}".format(reg), end="")
         elif lbyte == 0x65:
             # Fx65 - LD Vx, [I]
-            print("LD\tV{:x}, [I]".format(reg))
+            print("LD\tV{:x}, [I]".format(reg), end="")
         else:
             print()
 
     else:
-        print("unhandled opcode")
+        print("unhandled opcode", end="")
+    
+    # print address and opcode as comment
+    print("   \t; {:04x} :: {:02x} {:02x}\t".format(opaddr, hbyte, lbyte))
+
 
 def disasm(mem, size):
     print("disasm of: {}".format(filename))
     i = 0x0200
     while i < size:
-        print("{:04x} :: ".format(i), end="")
-        decode(mem[i], mem[i+1])
+        #print("{:04x} :: ".format(i), end="")
+        decode(mem[i], mem[i+1], i)
         i = i + 2
     
 
